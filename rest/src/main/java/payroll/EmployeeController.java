36bb8c8b58b4fb65973a2b7c1bc65a520467c8c0
root-marco <rootslowed@gmail.com>
2021-08-31 17:27:28 -0300

    rest services

---
+++ rest/src/main/java/payroll/EmployeeController.java
@@ -0,0 +1,81 @@
+package payroll;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+  private final EmployeeRepository repository;
+
+  public EmployeeController(EmployeeRepository repository) {
+    this.repository = repository;
+  }
+
+  @GetMapping("/employees")
+  public CollectionModel<EntityModel<Employee>> all() {
+
+    List<EntityModel<Employee>> employees = repository.findAll().stream()
+      .map(employee -> EntityModel.of(employee,
+        linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+        linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+      .collect(Collectors.toList());
+
+    return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
+
+  }
+
+  @PostMapping("/employees")
+  public Employee newEmployee(@RequestBody Employee newEmployee) {
+
+    return repository.save(newEmployee);
+
+  }
+
+  @GetMapping("/employees/{id}")
+  public EntityModel<Employee> one(@PathVariable Long id) {
+
+    Employee employee = repository.findById(id)
+      .orElseThrow(() -> new EmployeeNotFoundException(id));
+
+    return EntityModel.of(employee,
+      linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+      linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+
+  }
+
+  @PutMapping("/employees/{id}")
+  public Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+
+    return repository.findById(id)
+      .map(employee -> {
+        employee.setName(newEmployee.getName());
+        employee.setRole(newEmployee.getRole());
+        return repository.save(employee);
+      }) //
+      .orElseGet(() -> {
+        newEmployee.setId(id);
+        return repository.save(newEmployee);
+      });
+  }
+
+  @DeleteMapping("/employees/{id}")
+  public void deleteEmployee(@PathVariable Long id) {
+
+    repository.deleteById(id);
+
+  }
+
+}
---
+++ rest/src/main/java/payroll/EmployeeNotFoundException.java
@@ -0,0 +1,11 @@
+package payroll;
+
+public class EmployeeNotFoundException extends RuntimeException {
+
+	public EmployeeNotFoundException(Long id) {
+
+		super("Could not find employee " + id);
+
+	}
+
+}
